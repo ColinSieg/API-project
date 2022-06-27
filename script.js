e05d0b0e01eb2f7ad8012ba6ba29eadf2b21a05f
Colin Sieg <colin@trumpetsiege.com>
2022-06-27 18:38:56 -0500

    Added better buttons

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // NASA API key = HuEXLBHbanki6UBwsARsbTaUZZBa8Qm1IdIg0dRn
 
 const podButton = document.querySelector('.podButton')
-const randButton = document.querySelector('.randButton')
+const learnMore = document.querySelector('.learnMore')
 const imgDiv = document.querySelector('.imgDiv')
 
 podButton.addEventListener('click', async () => {
@@ -9,23 +9,7 @@ podButton.addEventListener('click', async () => {
     `https://api.nasa.gov/planetary/apod?api_key=HuEXLBHbanki6UBwsARsbTaUZZBa8Qm1IdIg0dRn`
   )
   let pic = response.data.url
-  imgDiv.innerHTML = `<img class="imgSmall" src=${pic}>`
+  let explain = response.data.explanation
+  imgDiv.innerHTML = `<figure><img class="imgFull" src=${pic}><figcaption>${explain}</figcaption></figure>`
+  console.log(response.data)
 })
-
-imgDiv.addEventListener('mouseenter', async () => {
-  let response = await axios.get(
-    `https://api.nasa.gov/planetary/apod?api_key=HuEXLBHbanki6UBwsARsbTaUZZBa8Qm1IdIg0dRn`
-  )
-  let pic = response.data.url
-  imgDiv.innerHTML = `<img class="imgFull" src=${pic}>`
-})
-
-imgDiv.addEventListener('mouseleave', async () => {
-  let response = await axios.get(
-    `https://api.nasa.gov/planetary/apod?api_key=HuEXLBHbanki6UBwsARsbTaUZZBa8Qm1IdIg0dRn`
-  )
-  let pic = response.data.url
-  imgDiv.innerHTML = `<img class="imgSmall" src=${pic}>`
-})
-
-// randButton.addEventListener('click', async () => {})
