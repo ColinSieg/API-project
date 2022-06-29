e17d85a6683f3a7eff69a0faf166c2788a81d77f
Colin Sieg <colin@trumpetsiege.com>
2022-06-28 19:14:58 -0500

    Added media types for image/video

--- script.js
+++ script.js
@@ -2,13 +2,19 @@
 
 const podButton = document.querySelector('.podButton')
 const learnMore = document.querySelector('.learnMore')
-const imgDiv = document.querySelector('.imgDiv')
+const mediaDiv = document.querySelector('.mediaDiv')
 
 podButton.addEventListener('click', async () => {
   let response = await axios.get(
     `https://api.nasa.gov/planetary/apod?api_key=HuEXLBHbanki6UBwsARsbTaUZZBa8Qm1IdIg0dRn`
   )
-  let pic = response.data.url
+  let title = response.data.title
+  let media = response.data.url
   let explain = response.data.explanation
-  imgDiv.innerHTML = `<figure><img class="imgFull" src=${pic}><figcaption>${explain}</figcaption></figure>`
+  let type = response.data.media_type
+  if (type === 'video') {
+    mediaDiv.innerHTML = `<figure><h1 class="txtCenter">${title}</h1><iframe width="840px" height="630" class="media" src=${media}></iframe><figcaption>${explain}</figcaption></figure>`
+  } else if (type === 'image') {
+    mediaDiv.innerHTML = `<figure><h1 class="txtCenter">${title}</h1><img class="media" src=${media}><figcaption>${explain}</figcaption></figure>`
+  }
 })
--- script2.js
+++ script2.js
@@ -1,14 +1,20 @@
 // NASA API key = HuEXLBHbanki6UBwsARsbTaUZZBa8Qm1IdIg0dRn
 
 const dateButton = document.querySelector('.dateButton')
-const imgDiv = document.querySelector('.imgDiv')
+const mediaDiv = document.querySelector('.mediaDiv')
 
 dateButton.addEventListener('click', async () => {
   let date = document.getElementById('search').value
   let response = await axios.get(
     `https://api.nasa.gov/planetary/apod?api_key=HuEXLBHbanki6UBwsARsbTaUZZBa8Qm1IdIg0dRn&date=${date}`
   )
-  let pic = response.data.url
+  let title = response.data.title
+  let media = response.data.url
   let explain = response.data.explanation
-  imgDiv.innerHTML = `<figure><img class="imgFull" src=${pic}><figcaption>${explain}</figcaption></figure>`
+  let type = response.data.media_type
+  if (type === 'video') {
+    mediaDiv.innerHTML = `<figure><h1 class="txtCenter">${title}</h1><iframe width="840px" height="630" class="media" src=${media}></iframe><figcaption>${explain}</figcaption></figure>`
+  } else if (type === 'image') {
+    mediaDiv.innerHTML = `<figure><h1 class="txtCenter">${title}</h1><img class="media" src=${media}><figcaption>${explain}</figcaption></figure>`
+  }
 })
