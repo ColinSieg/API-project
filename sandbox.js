2793c695c68ddfd9be13c5d4c722052f2da1f0d5
Colin Sieg <colin@trumpetsiege.com>
2022-07-03 16:02:55 -0500

    Stable 7/3/22

--- sandbox.js
+++ sandbox.js
@@ -1,12 +1,15 @@
-const dateFunc = (start, end) => {
-  return new Date(
-    start.getTime() + Math.random() * (end.getTime() - start.getTime())
-  )
-}
+// const dateFunc = (start, end) => {
+//   return new Date(
+//     start.getTime() + Math.random() * (end.getTime() - start.getTime())
+//   )
+// }
 
-const randDate = dateFunc(new Date(1995, 6, 16), new Date())
+// const randDate = dateFunc(new Date(1995, 6, 16), new Date())
 
-const year = randDate.getFullYear()
-const month = ('0' + (randDate.getMonth() + 1)).slice(-2)
-const day = ('0' + randDate.getDate()).slice(-2)
-const fullDate = year + '-' + month + '-' + day
+// const year = randDate.getFullYear()
+// const month = ('0' + (randDate.getMonth() + 1)).slice(-2)
+// const day = ('0' + randDate.getDate()).slice(-2)
+// const fullDate = year + '-' + month + '-' + day
+
+const currentDate = new Date().toDateString()
+console.log(currentDate)
